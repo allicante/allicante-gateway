3e3bc6e2cf1bce5f1e30d8e9d80fb5688bed3085
Jens Reidel <jens@troet.org>
2020-06-06 21:38:21 +0200

    Echo WS server with heartbeat

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,94 @@
-fn main() {
-    println!("Hello, world!");
+use actix::prelude::*;
+use actix::{Actor, StreamHandler};
+use actix_web::{middleware, web, App, Error, HttpRequest, HttpResponse, HttpServer};
+use actix_web_actors::ws;
+use std::time::{Duration, Instant};
+
+/// How often heartbeat pings are sent
+const HEARTBEAT_INTERVAL: Duration = Duration::from_secs(5);
+/// How long before lack of client response causes a timeout
+const CLIENT_TIMEOUT: Duration = Duration::from_secs(10);
+
+/// do websocket handshake and start `MyWebSocket` actor
+async fn ws_index(r: HttpRequest, stream: web::Payload) -> Result<HttpResponse, Error> {
+    println!("{:?}", r);
+    let res = ws::start(Websocket::new(), &r, stream);
+    println!("{:?}", res);
+    res
+}
+
+/// Define http actor
+struct Websocket {
+    hb: Instant,
+}
+
+impl Actor for Websocket {
+    type Context = ws::WebsocketContext<Self>;
+
+    // Startup method, used for hearbeat
+    fn started(&mut self, ctx: &mut Self::Context) {
+        self.hb(ctx)
+    }
+}
+
+// Handle for websocket messages
+impl StreamHandler<Result<ws::Message, ws::ProtocolError>> for Websocket {
+    fn handle(&mut self, msg: Result<ws::Message, ws::ProtocolError>, ctx: &mut Self::Context) {
+        println!("WS: {:?}", msg);
+        match msg {
+            Ok(ws::Message::Ping(msg)) => {
+                self.hb = Instant::now();
+                ctx.pong(&msg);
+            }
+            Ok(ws::Message::Pong(_)) => {
+                self.hb = Instant::now();
+            }
+            Ok(ws::Message::Text(text)) => ctx.text(text),
+            Ok(ws::Message::Binary(bin)) => ctx.binary(bin),
+            Ok(ws::Message::Close(_)) => {
+                ctx.stop();
+            }
+            _ => ctx.stop(),
+        }
+    }
+}
+
+impl Websocket {
+    fn new() -> Self {
+        Self { hb: Instant::now() }
+    }
+
+    // Heartbeat sender and checker
+    fn hb(&self, ctx: &mut <Self as Actor>::Context) {
+        ctx.run_interval(HEARTBEAT_INTERVAL, |act, ctx| {
+            // check client heartbeats
+            if Instant::now().duration_since(act.hb) > CLIENT_TIMEOUT {
+                // heartbeat timed out
+                println!("Heartbeat timeout");
+
+                // stop actor
+                ctx.stop();
+
+                // avoid pinging
+                return;
+            }
+
+            ctx.ping(b"");
+        });
+    }
+}
+
+#[actix_rt::main]
+async fn main() -> std::io::Result<()> {
+    std::env::set_var("RUST_LOG", "actix_server=info,actix_web=info");
+    env_logger::init();
+
+    HttpServer::new(|| {
+        App::new()
+            .wrap(middleware::Logger::default())
+            .service(web::resource("/").route(web::get().to(ws_index)))
+    })
+    .bind("0.0.0.0:5718")?
+    .run()
+    .await
 }
